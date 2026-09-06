dbcfbf27a21994981323485e98beca4e4973b670
Adam Ułanowski <a.ulanowski98@gmail.com>
2026-09-06 21:18:21 +0200

    feat: configure AngularNodeAppEngine with allowed hosts and improve server listening log

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,10 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: ['adamulanowski.dev', 'www.adamulanowski.dev', 'localhost', 'adamulanowski_dev'],
+});
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -41,9 +44,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -52,13 +53,13 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  const port = Number(process.env['PORT']) || 4000;
+  app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express server listening on http://0.0.0.0:${port}`);
   });
 }
 
