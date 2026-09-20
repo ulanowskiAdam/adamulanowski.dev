e59eaad534ac80872aacba326965bb1bb9d5bcff
Adam Ułanowski <aulanowski98@gmail.com>
2026-09-20 17:41:28 +0200

    feat: add interactive portrait scene and enhance home page layout

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutPortrait } from '../../components/about-portrait/about-portrait';
 import { ExperienceHero } from '../../components/experience-hero/experience-hero';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ExperienceHero],
+  imports: [ExperienceHero, AboutPortrait],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
